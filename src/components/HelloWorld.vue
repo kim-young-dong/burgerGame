f02038873bf125f75943a53487a7846950b4ae47
Mr.Kim <opa9904@gmail.com>
2022-01-23 21:55:00 +0900

    테스트 종료

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,5 @@
 <template>
   <div class="content" >
-    <img src="@/assets/logo.png" alt="">
-    <img src="@/public/logo.png" alt="">
     <div class="orderList">
       <div class="order" v-for="(menu, ind) in orderList"
       :key="ind">
